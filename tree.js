517e4d660d926ac8309ef31994a9baf690232e82
jhillgithub <jhillgithub@users.noreply.github.com>
2018-12-09 17:46:12 -0600

    cleaning up tree.js

--- tree.js
+++ tree.js
@@ -2,69 +2,73 @@ $ = document.querySelector.bind(document);
 $$ = document.querySelectorAll.bind(document);
 
 var tree = $(".tree");
-count = 0;
-for (var i = 1; i < 9; i++) {
+var count = 0;
 
-  var treeRow = document.createElement("div");
-  treeRow.classList.add('tree-row');
+function buildTree(rows = 9) {
+  for (var i = 1; i < rows; i++) {
+    var treeRow = document.createElement("div");
+    treeRow.classList.add("tree-row");
 
-  treeBranch = document.createElement("div");
-  treeBranch.classList.add('tree-branch');
+    var treeBranch = document.createElement("div");
+    treeBranch.classList.add("tree-branch");
 
-
-  for (var j = 0; j <= i; j++) {
-    count += 1;
-    console.log("count", count);
-    var bauble = htmlToElement(`
-    <div class="green-bauble">
+    for (var j = 0; j <= i; j++) {
+      var bauble = htmlToElement(`
+      <div class="green-bauble">
       <input type="checkbox" id="g${count}">
       <label for="g${count}">&nbsp;</label>
-    </div>`)
-    treeBranch.appendChild(bauble);
+      </div>`);
+      treeBranch.appendChild(bauble);
+      count += 1;
+    }
+
+    treeRow.appendChild(treeBranch);
+    tree.appendChild(treeRow);
   }
+}
+
+function buildBauble(elem, num = 3) {
+  for (var i = 0; i < 3; i++) {
+    var bauble = htmlToElement(`
+    <div class="green-bauble">
+    <input type="checkbox" id="g${count}">
+    <label for="g${count}">&nbsp;</label>
+    </div>`);
+    elem.appendChild(bauble);
+    count += 1;
+  }
+}
+
+function buildTrunk() {
+  /* Build 3 baubles, 1 trunk, 3 baubles for the bottom row */
+
+  var treeRow = document.createElement("div");
+  treeRow.classList.add("tree-row");
+
+  var treeBranch = document.createElement("div");
+  treeBranch.classList.add("tree-branch");
+
+  buildBauble(treeBranch);
+
+  var trunk = htmlToElement(`
+    <div class="trunk">
+    <img src="images/trunk.jpg" />
+    </div>`);
+
+  treeBranch.append(trunk);
+
+  buildBauble(treeBranch);
 
   treeRow.appendChild(treeBranch);
   tree.appendChild(treeRow);
-  console.log("End Loop", i);
 }
 
 function htmlToElement(html) {
-  var template = document.createElement('template');
-  html = html.trim(); // Never return a text node of whitespace as the result
+  var template = document.createElement("template");
+  html = html.trim();
   template.innerHTML = html;
   return template.content.firstChild;
 }
 
-var bauble = htmlToElement(`
-<div class="tree-row">
-  <div class="tree-branch">
-    <div class="green-bauble">
-      <input type="checkbox" id="g45">
-      <label for="g45">&nbsp;</label>
-    </div>
-    <div class="green-bauble">
-      <input type="checkbox" id="g46">
-      <label for="g46">&nbsp;</label>
-    </div>
-    <div class="green-bauble">
-      <input type="checkbox" id="g47">
-      <label for="g47">&nbsp;</label>
-    </div>
-    <div class="trunk">
-      <img src="images/trunk.jpg" />
-    </div>
-    <div class="green-bauble">
-      <input type="checkbox" id="g48">
-      <label for="g48">&nbsp;</label>
-    </div>
-    <div class="green-bauble">
-      <input type="checkbox" id="g49">
-      <label for="g49">&nbsp;</label>
-    </div>
-    <div class="green-bauble">
-      <input type="checkbox" id="g50">
-      <label for="g50">&nbsp;</label>
-    </div>
-  </div>
-</div>`)
-tree.appendChild(bauble);
+buildTree();
+buildTrunk();
